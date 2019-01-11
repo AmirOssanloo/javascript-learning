cda4eaff76a352a2c3704f28ee1dee47486b57a8
AmirOssanloo <amir@amirossanloo.com>
2019-01-11 13:03:48 +0100

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
   }, err => {
     res.status(400).send(err);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('Invalid user ID');
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo)
+      return res.status(404).send();
+
+    res.send({ todo });
+  }, err => {
+    res.status(400).send();
+  });
 });
 
 app.listen(3000, () => {
