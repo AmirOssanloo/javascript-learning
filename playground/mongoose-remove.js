532a35c47c5ce60ebbc37c692e236b7bb6515837
AmirOssanloo <amir@amirossanloo.com>
2019-01-12 12:31:49 +0100

    Added PATCH /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,6 +3,10 @@ const { mongoose } = require('../server/db/mongoose');
 const { Todo } = require('../server/models/todo');
 const { User } = require('../server/models/user');
 
-Todo.deleteMany({}).then(result => {
-  console.log(result);
+// Todo.deleteMany({}).then(result => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5c3898c48b7386ddb9832cf1').then(todo => {
+  console.log(todo);
 });
