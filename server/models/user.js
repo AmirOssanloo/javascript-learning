80caa4342067f821acad47d51212880433be40b9
AmirOssanloo <amir@amirossanloo.com>
2019-01-14 14:16:49 +0100

    Added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,16 @@ UserSchema.methods.generateAuthToken = function() {
   return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function(token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByCredentials = function(email, password) {
   let User = this;
 
