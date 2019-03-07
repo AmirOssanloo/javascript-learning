4156aa3875e0fea1e8b7a5a0937bac165a233329
AmirOssanloo <amir@amirossanloo.com>
2019-03-07 20:36:12 +0100

    Added middleware for user auth

--- nodejs-task-app/src/routers/user.js
+++ nodejs-task-app/src/routers/user.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const router = new express.Router();
+const auth = require('../middleware/auth');
 const User = require('../models/User');
+const router = new express.Router();
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-
-    res.status(200).send(users);
-  } catch (err) {
-    res.status(400).send(err);
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
@@ -36,6 +31,30 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token =>
+      token.token !== req.token);
+
+    await req.user.save();
+
+    res.send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
@@ -43,45 +62,37 @@ router.post('/users', async (req, res) => {
     await user.save();
     const token = await user.generateAuthToken();
 
-    res.status(200).send({user, token});
+    res.status(201).send({user, token});
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'age', 'password'];
   const isValidOperation = updates.every(update => allowedUpdates.includes(update));
 
   if (!isValidOperation)
-    return res.status(404).send({error: 'Invalid updates'});
+    return res.status(400).send({error: 'Invalid updates'});
 
   try {
-    const user = await User.findById(req.params.id);
-
-    updates.forEach(update => user[update] = req.body[update]);
-    await user.save();
+    updates.forEach(update =>
+      req.user[update] = req.body[update]);
     
-    if (!user)
-      return res.status(404).send();
-
-    res.status(200).send(user);
+    await req.user.save();
+    res.send(req.user);
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user)
-      return res.status(404).send();
-
-    return res.status(200).send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(500).send(err);
   }
 });
 
