aca4cebdef7a6df6f095b2c9987beac35818d176
AmirOssanloo <amir@amirossanloo.com>
2019-03-07 15:42:19 +0100

    Added user auth tokens

--- nodejs-task-app/src/routers/user.js
+++ nodejs-task-app/src/routers/user.js
@@ -25,13 +25,25 @@ router.get('/users/:id', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+
+    res.status(200).send({user, token});
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+    const token = await user.generateAuthToken();
 
-    res.status(200).send(user);
+    res.status(200).send({user, token});
   } catch (err) {
     res.status(400).send(err);
   }
