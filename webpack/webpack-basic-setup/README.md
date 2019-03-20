9bb1aad0eb2170584f8d2202169448ec828745cf
AmirOssanloo <amir@amirossanloo.com>
2019-03-20 12:38:56 +0100

    Added transforms and polyfills

--- webpack/webpack-basic-setup/README.md
+++ webpack/webpack-basic-setup/README.md
@@ -4,6 +4,11 @@ In this project we setup:
 * Webpack Dev Server
 * Hot Reloading
 * Better Errors
+* Debugging in DevTools
+* Babel Basics
+  * Plugins
+  * Polyfills
+  * Transforms
+  * Presets
 * Express Middleware
 * Server-side Reloading
-* Debugging in DevTools
--- webpack/webpack-basic-setup/src/main.js
+++ webpack/webpack-basic-setup/src/main.js
@@ -1,2 +1,11 @@
+require("@babel/runtime/regenerator");
 require("./main.css");
 require("./index.html");
+
+const a = async (args) => {
+  const {a, b} = args;
+  await console.log('Hello from the future');
+  console.log('Hello from the future');
+}
+
+a({a: 1, b: 2});
