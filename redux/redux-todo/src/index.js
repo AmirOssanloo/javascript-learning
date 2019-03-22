2454638c76d97475c1cf5829ff378eab25f5ba49
AmirOssanloo <amir@amirossanloo.com>
2019-03-22 17:39:22 +0100

    Added connect and action creators

--- redux/redux-todo/src/index.js
+++ redux/redux-todo/src/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
+
+let nextTodoId = 0;
 
 // Reducer (Handles individual todo)
 const todo = (state, action) => {
@@ -17,6 +20,7 @@ const todo = (state, action) => {
       if (state.id !== action.id) {
         return state;
       }
+
       return {
         ...state,
         completed: !state.completed
@@ -54,6 +58,199 @@ const visibilityFilter = (
   }
 }
 
+// Action creators
+// ============================
+const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  };
+};
+
+const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  };
+};
+
+const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+
+// React UI
+// ============================
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: (completed
+        ? 'line-through'
+        : 'none')
+    }}
+  >
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+let AddTodo = ({ dispatch }) => {
+  let input;
+
+  return (
+    <div>
+      <input ref={node => {
+          input = node;
+        }} />
+        <button onClick={() => {
+          dispatch(addTodo(input.value))
+          input.value = '';
+        }}>
+          Add Todo
+        </button>
+    </div>
+  )
+}
+AddTodo = connect()(AddTodo);
+
+const Link = ({
+  active,
+  children,
+  onClick
+}) => {
+  if (active) {
+    return <span>{children}</span>
+  }
+
+  return (
+    <a href='#'
+      onClick={e => {
+        e.preventDefault();
+        onClick();
+      }}
+    >
+      {children}
+    </a>
+  )
+}
+
+const mapStateToLinkProps = (
+  state,
+  ownProps
+) => {
+  return {
+    active: 
+      ownProps.filter ===
+      state.visibilityFilter
+  }
+}
+
+const mapDispatchToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibilityFilter(ownProps.filter));
+    }
+  }
+}
+
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
+
+const Footer = () => {
+  return (
+    <div>
+      Show:
+      <br />
+      <FilterLink filter='SHOW_ALL'>
+        All
+      </FilterLink>
+      <br />
+      <FilterLink filter='SHOW_ACTIVE'>
+        ACTIVE
+      </FilterLink>
+      <br />
+      <FilterLink filter='SHOW_COMPLETED'>
+        COMPLETED
+      </FilterLink>
+    </div>
+  )
+}
+
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed);
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed);
+  }
+}
+
+// Visible Todo List
+// ============================
+const mapStateToTodoListProps = state => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
+    )
+  };
+};
+
+const mapDispatchToTodoListProps = dispatch => {
+  return {
+    onTodoClick: id => {
+      store.dispatch(toggleTodo(id))
+    }
+  };
+};
+
+const VisibleTodoList = connect(
+  mapStateToTodoListProps,
+  mapDispatchToTodoListProps
+)(TodoList);
+
+// Main app component
+// ============================
+const TodoApp = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
+
 // Root state combines the state from several reducers
 const todoApp = combineReducers({
   todos,
@@ -63,48 +260,13 @@ const todoApp = combineReducers({
 // Create our Redux store
 const store = createStore(todoApp);
 
-// Adding some React UI
-let nextTodoId = 0;
-
-class TodoApp extends Component {
-  render() {
-    return (
-      <div>
-        <input ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={() => {
-          store.dispatch({
-            type: 'ADD_TODO',
-            text: this.input.value,
-            id: nextTodoId++
-          });
-
-          this.input.value = '';
-        }}>
-          Add Todo
-        </button>
-        <ul>
-          {this.props.todos.map(todo =>
-            <li key={todo.id}>
-              {todo.text}
-            </li>)}
-        </ul>
-      </div>
-    )
-  }
-}
-
-const render = () => {
-  ReactDOM.render(
-    <TodoApp 
-      todos={store.getState().todos}
-    />, document.getElementById('root')
-  );
-};
-
-store.subscribe(render);
-render();
+// Render the app
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>, document.getElementById('root')
+  
+);
 
 // Test for adding a new todo
 const testAddTodo = () => {
