d13fa6cbbe98ca5933342987573548d1b816275a
AmirOssanloo <amir@amirossanloo.com>
2019-05-08 12:06:02 +0200

    Trying out Formik

---
+++ react/react-formik/src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Formik } from 'formik';
+import styles from './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
