cf6f22de17e9249b340035c7a07e2b3000659ddd
AmirOssanloo <amir@amirossanloo.com>
2019-09-05 15:28:49 +0200

    Restructured Lerna Modules project

--- react/react-formik/src/components/app/App.jsx
+++ react/react-formik/src/components/app/App.jsx
@@ -1,95 +1,146 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-import ErrorList from "../error-list/ErrorList";
 import Input from "../input/Input";
 import styles from "./App.css";
 
-const loginInitialValues = {
-  email: ""
-  // password: ""
-};
+const App = ({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+  isSubmitting
+}) => (
+  <Form>
+    <Field
+      name="email"
+      type="email"
+      value={values.email}
+      touched={touched.email}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      component={Input}
+    />
+    <p>{errors.email}</p>
+    <button type="submit" disabled={isSubmitting}>
+      Submitting
+    </button>
+  </Form>
+);
 
-const loginValidationSchema = Yup.object().shape({
-  email: Yup.string()
-    .min(6, "Please enter at least 6 characters")
-    .max(40, "Please enter no more than 40 characters")
-    .email("Please enter a valid email")
-    .required("This field is required")
-  // password: Yup.string()
-  //   .min(6, "Please enter at least 6 characters")
-  //   .max(40, "Please enter no more than 40 characters")
-  //   .required("Please enter a password")
-});
+const FormikApp = withFormik({
+  mapPropsToValues: ({ name }) => ({
+    name: ""
+  }),
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .min(6, "Please enter at least 6 characters")
+      .max(40, "Please enter no more than 40 characters")
+      .email("Please enter a valid email")
+      .required("This field is required")
+  }),
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  }
+})(App);
 
-const loginSubmit = (values, setSubmitting) => {
-  setTimeout(() => {
-    alert(JSON.stringify(values, null, 2));
-    setSubmitting(false);
-  }, 400);
-};
+export default FormikApp;
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Formik
-        initialValues={loginInitialValues}
-        validationSchema={loginValidationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          loginSubmit(values, setSubmitting);
-        }}
-      >
-        {props => {
-          const {
-            touched,
-            errors,
-            handleChange,
-            handleBlur,
-            isSubmitting
-          } = props;
-          return (
-            <Form>
-              <Field
-                name="email"
-                type="email"
-                touched={touched.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                component={Input}
-              />
-              <ErrorMessage
-                name="email"
-                errors={errors.email}
-                component={ErrorList}
-              />
-              <button type="submit" disabled={isSubmitting}>
-                Submitting
-              </button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </div>
-  );
-};
+// const loginInitialValues = {
+//   email: ""
+//   // password: ""
+// };
 
-export default App;
+// const loginValidationSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .min(6, "Please enter at least 6 characters")
+//     .max(40, "Please enter no more than 40 characters")
+//     .email("Please enter a valid email")
+//     .required("This field is required")
+//   // password: Yup.string()
+//   //   .min(6, "Please enter at least 6 characters")
+//   //   .max(40, "Please enter no more than 40 characters")
+//   //   .required("Please enter a password")
+// });
 
-{
-  /* <Field
-name="password"
-type="password"
-touched={touched}
-onChange={handleChange}
-onBlur={handleBlur}
-component={Input}
-/>
-<ErrorMessage
-name="password"
-errors={errors.password}
-onChange={handleChange}
-onBlur={handleBlur}
-component={ErrorList}
-/> */
-}
+// const loginSubmit = (values, setSubmitting) => {
+//   setTimeout(() => {
+//     alert(JSON.stringify(values, null, 2));
+//     setSubmitting(false);
+//   }, 400);
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <Formik
+//         initialValues={loginInitialValues}
+//         validationSchema={loginValidationSchema}
+//         onSubmit={(values, { setSubmitting }) => {
+//           loginSubmit(values, setSubmitting);
+//         }}
+//       >
+//         {props => {
+//           const {
+//             values,
+//             touched,
+//             errors,
+//             handleChange,
+//             handleBlur,
+//             handleSubmit,
+//             isSubmitting
+//           } = props;
+//           return (
+//             <Form onSubmit={handleSubmit}>
+//               <Field
+//                 name="email"
+//                 type="email"
+//                 value={values.email}
+//                 touched={touched.email}
+//                 onChange={handleChange}
+//                 onBlur={handleBlur}
+//                 component={Input}
+//               />
+//               <p>{errors.email}</p>
+//               <pre>{JSON.stringify(errors, null, 2)}</pre>
+//               <ErrorMessage
+//                 name="email"
+//                 errors={errors.email}
+//                 component={ErrorList}
+//               />
+//               <button type="submit" disabled={isSubmitting}>
+//                 Submitting
+//               </button>
+//               <pre>{JSON.stringify(props, null, 2)}</pre>
+//             </Form>
+//           );
+//         }}
+//       </Formik>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// {
+//   /* <Field
+// name="password"
+// type="password"
+// touched={touched}
+// onChange={handleChange}
+// onBlur={handleBlur}
+// component={Input}
+// />
+// <ErrorMessage
+// name="password"
+// errors={errors.password}
+// onChange={handleChange}
+// onBlur={handleBlur}
+// component={ErrorList}
+// /> */
+// }
