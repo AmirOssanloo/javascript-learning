0385fbfd0906d943e7291731158515f3ab3b1e80
AmirOssanloo <amir@amirossanloo.com>
2020-01-23 11:29:36 -0300

    Setting up PassportJS

--- projects/feedbacker-shvc6il86j/server/index.js
+++ projects/feedbacker-shvc6il86j/server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./env/keys');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}));
+
+// app.use(passport);
 
 app.listen(PORT, () => {
   console.log(`Running server on port ${PORT}.`);
