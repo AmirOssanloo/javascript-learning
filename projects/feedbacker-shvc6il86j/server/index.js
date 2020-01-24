518fac23689c07fcbd0794e01d6f7e17d3f4645d
AmirOssanloo <amir@amirossanloo.com>
2020-01-24 19:18:28 -0300

    OAuth is now working

--- projects/feedbacker-shvc6il86j/server/index.js
+++ projects/feedbacker-shvc6il86j/server/index.js
@@ -1,9 +1,29 @@
+const cookieSession = require('cookie-session');
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const { cookieSecret, mongoURI } = require('./env/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-require('./routes/authRoutes')(app);
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [ cookieSecret ]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`Running server on port ${PORT}.`);
