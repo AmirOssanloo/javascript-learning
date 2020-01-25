6752a9f4df8e267050514ba59af279d3e2cfb483
AmirOssanloo <amir@amirossanloo.com>
2020-01-25 15:20:34 -0300

    Added dev and prod environments

---
+++ projects/feedbacker-shvc6/server/keys/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookieSecret: process.env.COOKIE_SECRET,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI
+};
