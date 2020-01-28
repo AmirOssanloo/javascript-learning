d3030f21785dd616ad38ca2dae9e944cce7b7e7b
AmirOssanloo <amir@amirossanloo.com>
2020-01-27 23:14:50 -0300

    Changing to Material UI for styling

--- projects/feedbacker-shvc6/server/client/src/components/Header.js
+++ projects/feedbacker-shvc6/server/client/src/components/Header.js
@@ -2,14 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <a href="/" className="left brand-logo">Feedbacker</a>
-        <ul className="right">
-          <li><a href="/auth/google">Login with Google</a></li>
-        </ul>
-      </div>
-    </nav>
+    <div></div>
   );
 };
 
