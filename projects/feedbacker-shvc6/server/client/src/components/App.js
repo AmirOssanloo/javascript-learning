f2f249448846640c667ccef58439e897312201fe
AmirOssanloo <amir@amirossanloo.com>
2020-01-29 20:41:27 -0300

    Added landing page

--- projects/feedbacker-shvc6/server/client/src/components/App.js
+++ projects/feedbacker-shvc6/server/client/src/components/App.js
@@ -4,11 +4,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 
 import Header from './Header';
+import Landing from './Landing';
 import * as actions from '../store/actions';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 const App = ({ fetchUser }) => {
 
--- projects/feedbacker-shvc6/server/client/src/components/Header.js
+++ projects/feedbacker-shvc6/server/client/src/components/Header.js
@@ -4,11 +4,10 @@ import { AppBar, Toolbar, Link, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => {
-  console.log(theme);
   return {
     toolbar: {
       display: 'flex',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
     },
     link: {
       margin: theme.spacing(1, 1.5)
@@ -41,7 +40,7 @@ const Header = ({ auth }) => {
   };
 
   return (
-    <AppBar color="default" elevation={0}>
+    <AppBar position="static" color="default" elevation={0}>
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5">
           <Link href="/" color="inherit">Feedbacker</Link>
