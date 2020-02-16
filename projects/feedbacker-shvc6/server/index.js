f470aec092fa60f4ba573e4151e930edd6ea01af
AmirOssanloo <amir@amirossanloo.com>
2020-02-16 18:59:16 -0300

    Added Heroku postinstall

--- projects/feedbacker-shvc6/server/index.js
+++ projects/feedbacker-shvc6/server/index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const passport = require('passport');
 const { cookieSecret, mongoURI } = require('./keys');
 const authRoutes = require('./routes/authRoutes');
@@ -28,6 +29,14 @@ app.use(passport.session());
 
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Running server on port ${PORT}.`);
